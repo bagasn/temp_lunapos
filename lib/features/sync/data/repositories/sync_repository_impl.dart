03adea6999fdfcbdc0608a7957708a10343cc975
bagasn <bagasnasution@gmail.com>
2026-09-05 17:54:23 +0700

    checkpoint 2

--- lib/features/sync/data/repositories/sync_repository_impl.dart
+++ lib/features/sync/data/repositories/sync_repository_impl.dart
@@ -2,12 +2,15 @@ import 'package:dartz/dartz.dart';
 import 'package:drift/drift.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pos/core/database/app_database_manager.dart';
+import 'package:pos/core/database/main_database.dart';
 import 'package:pos/core/local_storage/session_manager.dart';
 import 'package:pos/features/sync/data/datasources/sync_data_service.dart';
 import 'package:pos/features/sync/data/models/payload/sync_payload.dart';
+import 'package:pos/features/sync/data/models/response/initial_data_response.dart';
 import 'package:pos/features/sync/domain/models/sync_entity.dart';
 import 'package:pos/features/sync/domain/repositories/sync_repository.dart';
 import 'package:pos/shared/domain/entities/failure.dart';
+import 'package:pos/shared/utilities/log_util.dart';
 
 @LazySingleton(as: SyncRepository)
 class SyncRepositoryImpl implements SyncRepository {
@@ -17,27 +20,40 @@ class SyncRepositoryImpl implements SyncRepository {
 
   SyncRepositoryImpl(this._syncService, this._session, this._databaseManager);
 
-  Future<Either<Failure, SyncEntity>> getInitialData({
-    required int outletId,
-    required int companyId,
+  Future<Either<Failure, SyncEntity>> getInitialData(
+    SyncEntity params, {
     String? serverTime,
   }) async {
     try {
-      final posResult = await _syncService.getInitialData(
+      final outletFuture = _syncService.getInitialData(
         SyncPayload(serverTime: serverTime),
       );
+      // final productFuture = _syncService.getInitialProduct(
+      //   SyncPayload(serverTime: serverTime),
+      // );
+      // final promoFuture = _syncService.getInitialPromo(
+      //   SyncPayload(serverTime: serverTime),
+      // );
+
+      // final (outletResult, productResult, promoResult) = await (
+      //   outletFuture,
+      //   productFuture,
+      //   promoFuture,
+      // ).wait;
+
+      final outletResult = await outletFuture;
 
       final database = await _databaseManager.mainDb;
-      if (database != null) {
-        // TODO: Insert data to database
-      } else {
-        throw DatabaseFailure('Main Database not found');
+      if (database == null) {
+        throw DatabaseFailure('No connection to database has found!');
       }
+
+      _saveSyncOutlet(database, outletResult);
 
       return right(
         SyncEntity(
-          outletId: outletId,
-          companyId: companyId,
+          outletId: params.outletId,
+          companyId: params.companyId,
           outletSynced: false,
           productSynced: false,
           promoSynced: false,
@@ -49,50 +65,56 @@ class SyncRepositoryImpl implements SyncRepository {
       return left(NetworkFailure.error(error, stackTrace: stackTrace));
     }
   }
+}
 
-  @override
-  Future<Either<Failure, bool>> getInitialDataProduct({
-    String? serverTime,
-  }) async {
-    try {
-      final productResult = await _syncService.getInitialProduct(
-        SyncPayload(serverTime: serverTime),
-      );
+Future<bool> _saveSyncOutlet(
+  MainDatabase database,
+  InitialDataResponse data,
+) async {
+  try {
+    final customerInsert = <TableCustomerCompanion>[];
+    for (final item in data.customers) {
+      // final insert = TableCustomerCompanion.insert(
+      //   id: item.id,
+      //   username: null,
+      //   displayName: item.displayName,
+      //   firstName: item.firstName,
+      //   lastName: item.lastName,
+      //   company: company,
+      //   phone: phone,
+      //   email: email,
+      //   gender: gender,
+      //   dateOfBirth: dateOfBirth,
+      //   address: address,
+      //   addressStreet1: addressStreet1,
+      //   addressCity: addressCity,
+      //   addressStateProvince: addressStateProvince,
+      //   addressCountryId: addressCountryId,
+      //   addressCountryName: addressCountryName,
+      //   addressPostalCode: addressPostalCode,
+      //   bankAccountId: bankAccountId,
+      //   bankId: bankId,
+      //   bankName: bankName,
+      //   bankAccountName: bankAccountName,
+      //   bankAccountNumber: bankAccountNumber,
+      //   pricingCategoryId: pricingCategoryId,
+      //   isSync: isSync,
+      //   syncDate: syncDate,
+      //   createdByUserId: createdByUserId,
+      //   createdDateTime: createdDateTime,
+      //   lastUpdatedByUserId: lastUpdatedByUserId,
+      //   lastUpdateDateTime: lastUpdateDateTime,
+      //   deleted: deleted,
+      // );
 
-      final database = await _databaseManager.mainDb;
-      if (database != null) {
-        // TODO: Insert data to database
-      } else {
-        throw DatabaseFailure('Main Database not found');
-      }
-      return right(true);
-    } on DatabaseFailure catch (error) {
-      return left(error);
-    } catch (error, stackTrace) {
-      return left(NetworkFailure.error(error, stackTrace: stackTrace));
+      // customerInsert.add(insert);
     }
+
+    database.tableCustomer.insertAll([]);
+  } catch (error, stackTrace) {
+    LogUtil.e(error.toString(), stackTrace: stackTrace);
+    rethrow;
   }
 
-  @override
-  Future<Either<Failure, bool>> getInitialDataPromo({
-    String? serverTime,
-  }) async {
-    try {
-      final promoResult = await _syncService.getInitialPromo(
-        SyncPayload(serverTime: serverTime),
-      );
-
-      final database = await _databaseManager.mainDb;
-      if (database != null) {
-        // TODO: Insert data to database
-      } else {
-        throw DatabaseFailure('Main Database not found');
-      }
-      return right(true);
-    } on DatabaseFailure catch (error) {
-      return left(error);
-    } catch (error, stackTrace) {
-      return left(NetworkFailure.error(error, stackTrace: stackTrace));
-    }
-  }
+  return true;
 }
--- lib/features/sync/domain/models/sync_entity.dart
+++ lib/features/sync/domain/models/sync_entity.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class SyncEntity {
   final int outletId;
   final int companyId;
@@ -16,5 +17,21 @@ class SyncEntity {
 
   bool isSameOutlet(SyncEntity other) {
     return (companyId == other.companyId && outletId == other.outletId);
+  }
+
+  SyncEntity copyWith({
+    int? outletId,
+    int? companyId,
+    bool? outletSynced,
+    bool? productSynced,
+    bool? promoSynced,
+  }) {
+    return SyncEntity(
+      outletId: outletId ?? this.outletId,
+      companyId: companyId ?? this.companyId,
+      outletSynced: outletSynced ?? this.outletSynced,
+      productSynced: productSynced ?? this.productSynced,
+      promoSynced: promoSynced ?? this.promoSynced,
+    );
   }
 }
